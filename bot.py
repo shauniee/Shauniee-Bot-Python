dcf562286c70838752f793289f27924491b42bfa
Shaun <shaunhoon03@gmail.com>
2022-07-21 12:01:11 +0800

    Added Welcome Message and Chatbot

--- bot.py
+++ bot.py
@@ -1,18 +1,49 @@
+from http import client
 import os
 import random
+from venv import create
 
 import discord
 from discord.ext import commands
+from discord.utils import get
 from dotenv import load_dotenv
 
-load_dotenv()
-# TOKEN = ('Add your bot token here')
+from neuralintents import GenericAssistant
 
-bot = commands.Bot(command_prefix = '/')
+chatbot = GenericAssistant('intents.json')
+chatbot.train_model()
+chatbot.save_model()
+
+client = discord.Client()
+
+load_dotenv()
+# TOKEN = ('Add your own token here')
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix = '/', intents = intents)
+
+
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member}, welcome to Shauns discord channel <3'
+    )
+
+@bot.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("Shauniee"):
+        response = chatbot.request(message.content[7:])
+        await message.channel.send(response)
 
 @bot.command(name = 'aoyu')
 async def call_aoyu(ctx):
-    call_ay = ("@tzuyu#9733 hi"),
+    call_ay = ("@tzuyu#9733  hi"),
 
     response = random.choice(call_ay)
     await ctx.send(response)
